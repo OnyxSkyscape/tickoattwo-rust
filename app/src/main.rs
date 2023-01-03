34bdbf96cc2f03ace287aa58092858cae7d91102
55nknown <annon@annon.me>
2023-01-03 18:56:35 +0100

    add footer

--- app/src/main.rs
+++ app/src/main.rs
@@ -66,54 +66,76 @@ fn App() -> Html {
     }
 
     html! {
-        <div class="flex flex-col justify-center items-center p-12 h-screen transition-all">
-            <div class={classes!(animate_logo)}>
-                <img src="/icon.svg" height=80 width=80 />
-            </div>
+        <div class="flex flex-col justify-between items-center h-screen">
+            <div></div>
+            <div class="flex flex-col justify-center items-center">
+                <div class={classes!(animate_logo)}>
+                    <img src="/icon.svg" height=80 width=80 />
+                </div>
 
-            if *state == GameState::Enter {
-                <>
-                    <div class="mt-4 mb-6">
-                        <span class="text-4xl font-bold">{"Welcome to TickOatTwo!"}</span>
-                    </div>
-                    <div class="mt-6 flex flex-col gap-2">
-                        <span class="text-gray-600 font-mono">{"Please enter a nickname"}</span>
-                        <div class="flex gap-4">
-                            <input ref={input_ref} {onchange} value={input_value} type="text" class="text-white font-mono bg-black border border-2 px-4 py-1 text-lg rounded-lg w-[300px]" />
-                            <div {onclick} class="bg-white text-black rounded-lg px-8 py-2 text-lg cursor-pointer select-none">
-                                <span>{"Enter"}</span>
+                if *state == GameState::Enter {
+                    <>
+                        <div class="mt-4 mb-6">
+                            <span class="text-4xl font-bold">{"Welcome to TickOatTwo!"}</span>
+                        </div>
+                        <div class="sep flex justify-center gap-2">
+                            <span class="w-1 h-1 bg-gray-700 rounded-full"></span>
+                            <span class="w-1 h-1 bg-gray-700 rounded-full"></span>
+                            <span class="w-1 h-1 bg-gray-700 rounded-full"></span>
+                        </div>
+                        <div class="mt-6 flex flex-col gap-2">
+                            <span class="text-gray-500 font-mono">{"Please enter a nickname"}</span>
+                            <div class="flex gap-4">
+                                <input ref={input_ref} {onchange} value={input_value} type="text" class="text-white font-mono bg-black border border-2 px-4 py-1 text-lg rounded-lg w-[300px]" />
+                                <div {onclick} class="bg-white text-black rounded-lg px-8 py-2 text-lg cursor-pointer select-none">
+                                    <span>{"Enter"}</span>
+                                </div>
                             </div>
                         </div>
+                    </>
+                }
+                if *state == GameState::Queued {
+                    <div class="mt-5 text-lg text-gray-400 flex gap-4 justify-center items-center">
+                        <svg class="spinner">
+                            <circle cx="20" cy="20" r="18"></circle>
+                        </svg>
+                        <span>{"Waiting for opponent..."}</span>
                     </div>
-                </>
-            }
-            if *state == GameState::Queued {
-                <div class="mt-5 text-lg text-gray-400 flex gap-4 justify-center items-center">
-                    <svg class="spinner">
-                        <circle cx="20" cy="20" r="18"></circle>
-                    </svg>
-                    <span>{"Waiting for opponent..."}</span>
+                }
+                if *state == GameState::Playing {
+                    <div class="flex flex-col mt-12 justify-center items-center">
+                        <div class="flex">
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                        </div>
+                        <div class="flex">
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                        </div>
+                        <div class="flex">
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                            <div class="h-[100px] w-[100px] border border-4"></div>
+                        </div>
+                    </div>
+                }
+            </div>
+            <div class="footer flex border-t-2 border-gray-800 justify-between items-center px-3 py-2 pt-1 w-screen">
+                <div class="flex flex-row gap-2 items-center">
+                    <div class="flex justify-center items-center">
+                        <div class="flex h-3 w-3 relative">
+                            <div class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></div>
+                            <div class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></div>
+                        </div>
+                    </div>
+                    <span>{"0 players online"}</span>
                 </div>
-            }
-            if *state == GameState::Playing {
-                <div class="flex flex-col mt-12 justify-center items-center">
-                    <div class="flex">
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                    </div>
-                    <div class="flex">
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                    </div>
-                    <div class="flex">
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                        <div class="h-[100px] w-[100px] border border-4"></div>
-                    </div>
+                <div class="flex flex-row gap-2 items-center">
+                    <a href="http://github.com/OnyxSkyscape/tickoattwo-rust" target="_blank">{"source"}</a>
                 </div>
-            }
+            </div>
         </div>
     }
 }
