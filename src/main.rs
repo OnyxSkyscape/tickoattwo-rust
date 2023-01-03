1bba8d3a06d479247350d3c1edbd43a77d51dfb0
55nknown <annon@annon.me>
2023-01-03 07:33:57 +0100

    add http server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::io::Error as IoError;
+use std::env;
 
-use tickoattwo_rust::network::serve;
 use tickoattwo_rust::backend::Backend;
+use tickoattwo_rust::network::serve;
 
 #[tokio::main]
-async fn main() -> Result<(), IoError> {
+async fn main() -> Result<(), hyper::Error> {
+    let addr = env::args()
+        .nth(1)
+        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+
     let backend = Backend::new();
-    serve(backend).await?;
+    serve(backend, addr).await?;
     Ok(())
 }
--- src/network.rs
+++ src/network.rs
@@ -1,12 +1,19 @@
 use futures::SinkExt;
 use futures_util::StreamExt;
 
-use tokio::net::{TcpListener, TcpStream};
-use tungstenite::{Error, Result};
+use hyper::{
+    header::{CONNECTION, SEC_WEBSOCKET_ACCEPT, SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_VERSION, UPGRADE},
+    http::HeaderValue,
+    server::conn::AddrStream,
+    service::{make_service_fn, service_fn},
+    upgrade::Upgraded,
+    Body, Method, Request, Response, Server, StatusCode, Version,
+};
+use tokio_tungstenite::WebSocketStream;
+use tungstenite::{handshake::derive_accept_key, protocol::Role, Error, Result};
 
 use std::{
-    env,
-    io::Error as IoError,
+    convert::Infallible,
     net::SocketAddr,
     sync::{Arc, Mutex},
 };
@@ -15,14 +22,9 @@ use crate::{backend::Backend, packet::Packet};
 
 async fn handle_connection(
     backend: Arc<Mutex<Backend>>,
-    raw_stream: TcpStream,
+    ws_stream: WebSocketStream<Upgraded>,
     addr: SocketAddr,
 ) -> Result<()> {
-    println!("Incoming TCP connection from: {}", addr);
-
-    let ws_stream = tokio_tungstenite::accept_async(raw_stream)
-        .await
-        .expect("Error during the websocket handshake occurred");
     println!("WebSocket connection established: {}", addr);
 
     let (mut tx, mut rx) = ws_stream.split();
@@ -66,29 +68,90 @@ async fn handle_connection(
     Ok(())
 }
 
-async fn accept_connection(backend: Arc<Mutex<Backend>>, peer: SocketAddr, stream: TcpStream) {
-    if let Err(e) = handle_connection(backend, stream, peer).await {
-        match e {
-            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
-            err => println!("Error processing connection: {}", err),
-        }
+async fn handle_request(
+    backend: Arc<Mutex<Backend>>,
+    mut req: Request<Body>,
+    addr: SocketAddr,
+) -> Result<Response<Body>, Infallible> {
+    println!("{}: {}", req.method().as_str(), req.uri().path());
+
+    let upgrade = HeaderValue::from_static("Upgrade");
+    let websocket = HeaderValue::from_static("websocket");
+    let headers = req.headers();
+    let key = headers.get(SEC_WEBSOCKET_KEY);
+    let derived = key.map(|k| derive_accept_key(k.as_bytes()));
+    if req.method() != Method::GET
+        || req.version() < Version::HTTP_11
+        || !headers
+            .get(CONNECTION)
+            .and_then(|h| h.to_str().ok())
+            .map(|h| {
+                h.split(|c| c == ' ' || c == ',')
+                    .any(|p| p.eq_ignore_ascii_case(upgrade.to_str().unwrap()))
+            })
+            .unwrap_or(false)
+        || !headers
+            .get(UPGRADE)
+            .and_then(|h| h.to_str().ok())
+            .map(|h| h.eq_ignore_ascii_case("websocket"))
+            .unwrap_or(false)
+        || !headers
+            .get(SEC_WEBSOCKET_VERSION)
+            .map(|h| h == "13")
+            .unwrap_or(false)
+        || key.is_none()
+        || req.uri() != "/api/ws"
+    {
+        return Ok(Response::new(Body::from("Hello World!")));
     }
+
+    let ver = req.version();
+
+    tokio::task::spawn(async move {
+        match hyper::upgrade::on(&mut req).await {
+            Ok(upgraded) => {
+                if let Err(e) = handle_connection(
+                    backend,
+                    WebSocketStream::from_raw_socket(upgraded, Role::Server, None).await,
+                    addr,
+                )
+                .await
+                {
+                    match e {
+                        Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+                        err => println!("Error processing connection: {}", err),
+                    }
+                }
+            }
+            Err(e) => println!("upgrade error: {}", e),
+        }
+    });
+
+    let mut res = Response::new(Body::empty());
+    *res.status_mut() = StatusCode::SWITCHING_PROTOCOLS;
+    *res.version_mut() = ver;
+    res.headers_mut().append(CONNECTION, upgrade);
+    res.headers_mut().append(UPGRADE, websocket);
+    res.headers_mut()
+        .append(SEC_WEBSOCKET_ACCEPT, derived.unwrap().parse().unwrap());
+    Ok(res)
 }
 
-pub async fn serve(backend: Backend) -> Result<(), IoError> {
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
-
+pub async fn serve(backend: Backend, addr: String) -> Result<(), hyper::Error> {
     let backend = Arc::new(Mutex::new(backend));
 
-    let try_socket = TcpListener::bind(&addr).await;
-    let listener = try_socket.expect("Failed to bind");
-    println!("Listening on: {}", addr);
+    let make_svc = make_service_fn(move |conn: &AddrStream| {
+        let remote_addr = conn.remote_addr();
+        let backend = backend.clone();
+        let service = service_fn(move |req| handle_request(backend.clone(), req, remote_addr));
+        async { Ok::<_, Infallible>(service) }
+    });
 
-    while let Ok((stream, peer)) = listener.accept().await {
-        tokio::spawn(accept_connection(Arc::clone(&backend), peer, stream));
-    }
+    let server = Server::bind(&addr.parse().unwrap()).serve(make_svc);
 
-    Ok(())
+    println!("Listening on {}", addr);
+
+    server.await?;
+
+    Ok::<_, hyper::Error>(())
 }
